e85250cab41b8155e54a659373702ee2c07741c4
bulend-karadag <bulendkaradag@gmail.com>
2022-09-09 23:07:51 -0300

    sql commands

--- sql_app/database.py
+++ sql_app/database.py
@@ -5,11 +5,11 @@ import pymysql
 
 SQLALCHEMY_DATABASE_URL = 'mysql+pymysql://user:password@localhost:3306/LibGen'
 #SQLALCHEMY_DATABASE_URL = "sqlite:////sql_app/libgen.db"
-# SQLALCHEMY_DATABASE_URL = "postgresql://user:password@postgresserver/db"
+#SQLALCHEMY_DATABASE_URL = "postgresql://user:password@postgresserver/db"
 
 engine = create_engine(
     #SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
-    SQLALCHEMY_DATABASE_URL, encoding='utf8', echo=True
+    SQLALCHEMY_DATABASE_URL, encoding='utf8', echo=False
 )
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
